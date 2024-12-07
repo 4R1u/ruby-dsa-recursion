446dee9bae069afa893eb79d5fc94ec001714685
4R1u <62248461+4R1u@users.noreply.github.com>
2024-12-07 10:30:28 +0400

    Implement fibs_rec

--- fibs.rb
+++ fibs.rb
@@ -6,3 +6,11 @@ def fibs(num)
   (num - 2).times { |n| array << array[n] + array[n + 1] }
   array[0...num]
 end
+
+def fibs_rec(num)
+  num = num.abs
+  return [0, 1, 1][0...num] if num <= 2
+
+  array = fibs_rec(num - 1)
+  array << array[-1] + array[-2]
+end
